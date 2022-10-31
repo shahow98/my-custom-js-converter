c975580bd6848a43b9c7bb72bb7f511dcaf2b2cf
kezh <kezh@ffcs.cn>
2022-10-31 14:27:19 +0800

    fix: 扫描导入文件异常

--- src/scanf/index.ts
+++ src/scanf/index.ts
@@ -2,22 +2,21 @@ import fs from "fs";
 import path from "path";
 
 export function scanfCodeFiles(
-  dirPaths: string|string[],
+  dirPaths: string | string[],
   filterName: string = ""
 ): string[] {
- if(dirPaths instanceof String) {
-  return scanfCodeFiles$0(dirPaths as string, filterName);
- }
- if(dirPaths instanceof Array<string>){
-  return (dirPaths as string[]).flatMap(p => scanfCodeFiles$0(p, filterName));
- }
- return [];
+  if (dirPaths instanceof String) {
+    return scanfCodeFiles$0(dirPaths as string, filterName);
+  }
+  if (dirPaths instanceof Array<string>) {
+    return (dirPaths as string[]).flatMap((p) =>
+      scanfCodeFiles$0(p, filterName)
+    );
+  }
+  return [];
 }
 
-function scanfCodeFiles$0(
-  dirPath: string,
-  filterName: string = ""
-): string[] {
+function scanfCodeFiles$0(dirPath: string, filterName: string = ""): string[] {
   const filePaths: string[] = [];
   if (!fs.existsSync(dirPath)) {
     console.log(`路径不存在[${dirPath}]忽略`);
@@ -25,7 +24,7 @@ function scanfCodeFiles$0(
   }
   const files = fs.readdirSync(dirPath, {
     encoding: "utf-8",
-    withFileTypes: true,
+    withFileTypes: true
   });
   files
     .filter((file) => file.isFile())
@@ -54,23 +53,27 @@ export function scanfCodeDirs(baseDir: string, targetDirs: string[]): string[] {
  * 扫描引入路径
  * require('../../util')
  *   - is dir ../../util/index.js
- *   - is file ../../util.js 
+ *   - is file ../../util.js
  *   - throw err
  * @param modPath - 引入路径
- * @returns 
+ * @returns
  */
-export function scanfRequieMod(baseDir: string, modPath: string):string {
+export function scanfRequieMod(baseDir: string, modPath: string): string {
   let absolutePath = path.join(baseDir, modPath);
   try {
-    fs.accessSync(absolutePath, fs.constants.O_DIRECTORY);
-    absolutePath = path.join(absolutePath, 'index.js');
-    fs.accessSync(absolutePath, fs.constants.F_OK);
-  } catch(err) {
+    fs.accessSync(absolutePath, fs.constants.R_OK);
+  } catch (err) {
     try {
-      absolutePath = `${absolutePath}.js`;
+      fs.accessSync(absolutePath, fs.constants.O_DIRECTORY);
+      absolutePath = path.join(absolutePath, "index.js");
       fs.accessSync(absolutePath, fs.constants.F_OK);
-    } catch(err) {
-      throw new Error(`${absolutePath} is not exists!`);
+    } catch (err) {
+      try {
+        absolutePath = `${absolutePath}.js`;
+        fs.accessSync(absolutePath, fs.constants.F_OK);
+      } catch (err) {
+        throw new Error(`${absolutePath} is not exists!`);
+      }
     }
   }
   return absolutePath;
